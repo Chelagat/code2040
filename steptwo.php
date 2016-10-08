ce80e90e1d305e23171dc8fdc59711b558d31c3a
Chela <nborus@stanford.edu>
2016-10-08 13:46:01 -0700

    Made corrections

--- steptwo.php
+++ steptwo.php
@@ -7,14 +7,19 @@ $token = '3af306b2194e879055b41443412e4daf';
 $postData = array(
     'token' => $token,
 );
-
+$json_object = json_encode($postData);
 // Setup cURL
 $ch = curl_init('http://challenge.code2040.org/api/reverse');
-curl_setopt($ch, CURLOPT_POSTFIELDS ,json_encode($postData));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS ,$json_object);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($json_object))                                                                       
+);    
 //send request
 $response = curl_exec($ch);
+echo($response);
 //reverse string response
 $rev_response = strrev ($response);
 //data to send to API
